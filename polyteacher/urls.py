6219a1a1fae6f8e700584985264e8daff9c26c0a
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-12-05 13:00:20 +0100

    feat: add redoc lib

--- polyteacher/urls.py
+++ polyteacher/urls.py
@@ -20,10 +20,24 @@ from translator.views import index
 from translator.views import FrenchSpanishTranslationViewSet
 from translator.views import FrenchEnglishTranslationViewSet
 from translator.views import AllTranslation
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="PolyTeacher API",
+      default_version='v1',
+      description="Welcome to the world of Jaseci",
+      terms_of_service="https://www.adou.com"
+   ),
+   public=True
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/all_translations/', AllTranslation.as_view(), name='all_translations'),
-    path('', index),
-    path('api/french_spanish_translator/', FrenchSpanishTranslationViewSet.as_view(), name='french_spanish_translator'),
-    path('api/french_english_translator/', FrenchEnglishTranslationViewSet.as_view(), name='french_english_translator'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0)),
+    # path('api/french_spanish_translator/', FrenchSpanishTranslationViewSet.as_view(), name='french_spanish_translator'),
+    # path('api/french_english_translator/', FrenchEnglishTranslationViewSet.as_view(), name='french_english_translator'),
 ]
